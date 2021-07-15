70a02a034c4a51ccfef6f0139338c86890541523
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-07-16 02:57:25 +0500

    Update redis.py

--- hitsuki/services/redis.py
+++ hitsuki/services/redis.py
@@ -1,8 +1,5 @@
-# Copyright (C) 2018 - 2020 MrYacha. All rights reserved. Source code available under the AGPL.
-# Copyright (C) 2019 Aiogram
-#
-# This file is part of Hitsuki (Telegram Bot)
-#
+# This file is part of Utah (Telegram Bot)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
@@ -21,20 +18,20 @@ import sys
 import redis as redis_lib
 
 from hitsuki import log
-from hitsuki.config import get_str_key, get_int_key
+from hitsuki.config import get_str_key
 
 # Init Redis
-redis = redis_lib.StrictRedis(
+redis = redis_lib.Redis(
     host=get_str_key("REDIS_URI"),
     port=get_str_key("REDIS_PORT"),
-    db=get_int_key("REDIS_DB_FSM"),
-    decode_responses=True
+    password=get_str_key("REDIS_PASS"),
+    decode_responses=True,
 )
 
-bredis = redis_lib.StrictRedis(
+bredis = redis_lib.Redis(
     host=get_str_key("REDIS_URI"),
     port=get_str_key("REDIS_PORT"),
-    db=get_int_key("REDIS_DB_FSM")
+    password=get_str_key("REDIS_PASS"),
 )
 
 try:
